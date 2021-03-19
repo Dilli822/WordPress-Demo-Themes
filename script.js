dec5093988464fd0066c235395a05e4958a19994
dilli hang rai <dillihangrae@gmail.com>
2021-03-19 22:08:22 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/* this is array */
+var characters = ['dilli','hari','gautam','loreal'];
+console.log(characters);
+console.log(characters.length);
+
+myDiv.addEventListener('click', doSomething)
+function doSomething(ev){
+  console.log(ev.type)
+  console.log(ev.target)
+}
